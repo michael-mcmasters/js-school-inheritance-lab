aba4e4b539946a5bd7a62356e5937d28447ec458
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-01-04 08:24:09 -0500

    first checkin

---
+++ classes/person/Person.test.js
@@ -0,0 +1,12 @@
+const { TestScheduler } = require('jest');
+const Person = require('./Person');
+
+test("What is your name?", () => {
+
+    //let person = new Person();
+    let expected = "Bobby Digital";
+
+    // let actual = person.getName();
+
+    //expect(actual).toEqual(expected);
+});
