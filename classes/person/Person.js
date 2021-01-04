91f0920deaca85c7cb7a3ca71c88682a431c5fe3
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-04 12:42:59 -0500

    Completed a part of it

--- classes/person/Person.js
+++ classes/person/Person.js
@@ -1,5 +1,48 @@
-class Person {
-
+/**
+ * Creating the objects constructor, and setting the objects state
+ * @param {*} firstName 
+ * @param {*} lastName 
+ * @param {*} age 
+ * @param {*} gender 
+ * @param {*} interests 
+ */
+function Person(firstName, lastName, age, gender, interests) {
+  this.name = { firstName, lastName };
+  this.age = age;
+  this.gender = gender;
+  this.interests = interests;
 }
 
+Person.prototype.greeting = function () {
+  let response = "Hi, my name is " + this.name.firstName;
+  return response;
+}
+
+Person.prototype.bio = function () {
+  // Step 01 First define a string, and make it equal to the part of
+  // the bio that we know will always be the same.
+  // first name, lastname and age:
+  // This persons name name is Bobby Digital, and I am 51 years old.
+  // Add all of that to the varaiable you create called stringOut;
+
+  //Step 02 check what the value of gender is and set the pronoun to the approriate case
+  // example input for pronoun for a man can be [Male, male, M, m] set the value of a variable called pronoun to 'He likes'
+  // exmaple input for pronoun for a female can be [Female, female, F, f] set the value of a variable called pronoun to 'She likes'
+
+  //step 03 add the pronoun to a varaible called stringOut;
+
+  // step 04 if interest is an array then add to string all of the interest stringOut
+
+  // step 05 return stringOut
+
+  /**
+   * This persons is name is Bobby Digital and I am 51 years old.
+   * He likes making Phat beats, and Ill Rhymes
+   */
+}
+
+/**
+ * This is a behavior provided by node, that allows for us to create a 
+ * connection between class and functions defined in diffent files
+ */
 module.exports = Person;
--- classes/person/Person.test.js
+++ classes/person/Person.test.js
@@ -3,10 +3,28 @@ const Person = require('./Person');
 
 test("What is your name?", () => {
 
-    //let person = new Person();
-    let expected = "Bobby Digital";
+    let person = new Person("Bobby", "Digital", 51, "M", "Making Phat Beats");
+    let expected = "Bobby";
+    let actual = person.name.firstName;
 
-    // let actual = person.getName();
-
-    //expect(actual).toEqual(expected);
+    expect(actual).toEqual(expected);
 });
+
+test("Greeting", () => {
+    let person = new Person("Bobby", "Digital", 51, "M", "Making Phat Beats");
+    let expected = "Hi, my name is Bobby";
+    let actual = person.greeting();
+    expect(actual).toEqual(expected);
+});
+
+// test("Bio", () => {
+//     const person = new Person(Bobby, Digital, 51, "M', Making Phat Beats");
+//     const expected = "This persons is name is Bobby Digital and I am 51 years old. He likes making Phat beats, and Ill Rhymes";
+// });
+
+test("Array of interests ", () => {
+    let person = new Person("Sally", "Mae", 49, "F", ["Working Out ", "Building Cars", "Cooking"]);
+    let expected = " This persons name is Sally Mae and she is 49 years old. She likes Working Out, Building Cars, and Cooking"
+    let actual = person.bio();
+    expect(actual).toEqual(expected);
+})
