dbba3ab8f651f38efeaf4d2e3696bd5d4f544cea
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-06 12:41:35 -0500

    Comments

--- functions/person/Person.js
+++ functions/person/Person.js
@@ -1,5 +1,38 @@
-function Person(){
+class Person {
+  constructor(firstName, lastName, age, gender, interests) {
+    this.name = { firstName, lastName };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+  }
 
+  greeting() {
+    let response = "Hi, my name is " + this.name.firstName;
+    return response;
+  }
+
+  bio() {
+    let personPronoun = "They"
+    let inputGender = this.gender.toLowerCase();
+    if (inputGender === "f" || inputGender === "female") {
+      personPronoun = "She";
+    } else if (inputGender === "m" || inputGender === "male") {
+      personPronoun = "He";
+    }
+    let userInterest = "";
+    if (typeof this.interests === "string") {
+      userInterest = this.interests;
+    } else {
+      for (let i = 0; i < this.interests.length; i++) {
+        if (i === this.interests.length - 1) {
+          userInterest += "and " + this.interests[i];
+        } else {
+          userInterest += this.interests[i] + ", ";
+        }
+      }
+    }
+    let stringOut = `This persons name is ${this.name.firstName} ${this.name.lastName} and ${personPronoun.toLowerCase()} is ${this.age} years old. ${personPronoun} likes ${userInterest}.`;
+    return stringOut;
+  }
 }
-
 module.exports = Person;
